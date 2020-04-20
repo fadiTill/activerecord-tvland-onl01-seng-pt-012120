3ef0821816427b0f72e7a2770f54bac478f18631
fadimata tillman <fadi.tillman@gmail.com>
2020-04-20 04:57:23 +0000

    Automatically backed up by Learn

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -2,8 +2,8 @@ lass CreateCharacters < ActiveRecord::Migrations[5.2]
   def change
     create_table :characters do |t|
       t.string  :name
-      t.integer:actor_id
-      t.integer:show_id
+      t.integer :actor_id
+      t.integer :show_id
     end
   end
 end
