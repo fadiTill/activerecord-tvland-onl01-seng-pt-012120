1158f17619429a3dc446b116891de5e237f05cff
fadimata tillman <fadi.tillman@gmail.com>
2020-04-20 05:13:06 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,11 @@
 class Show < ActiveRecord::Base
+  has_many :character
+  has_many :actors, through: :characters
+  belongs_to :network
 
+      def actors_list
+       self.characters.collect {|c| Actor.find(c.actor_id).full_name}
+     end
+
+    end
 end
