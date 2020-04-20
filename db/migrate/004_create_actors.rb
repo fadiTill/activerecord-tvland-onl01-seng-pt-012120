e680be09796464c81c86200f70f6b05a45135f23
fadimata tillman <fadi.tillman@gmail.com>
2020-04-20 04:53:40 +0000

    Automatically backed up by Learn

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -1,8 +1,8 @@
 class CreateActors < ActiveRecord::Migrations[5.2]
   def change
     create_table :actors do |t|
-      first_name
-      last_name
+    t.string  :first_name
+      t.string:last_name
     end
   end
 end
