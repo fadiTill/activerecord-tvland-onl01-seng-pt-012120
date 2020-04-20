df116353d1e13e7a4d0ea232850b554daa49a052
fadimata tillman <fadi.tillman@gmail.com>
2020-04-20 05:00:47 +0000

    Automatically backed up by Learn

--- db/migrate/0006_create_catchphrases.rb
+++ db/migrate/0006_create_catchphrases.rb
@@ -1,3 +1,4 @@
-class CreateCatchphrases< ActiveRecord::Migrations[5.2]
+class CreateCatchphrases < ActiveRecord::Migrations[5.2]
   def change
     create_table :catchphrase < ActiveRecord::Migrations[5.2]
+    
